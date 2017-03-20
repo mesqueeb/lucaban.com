eb30a83aeccc53fc03c50473b737ff8d7378b94e
Luca Ban - Golden Birdy <mesqueeb@gmail.com>
2017-03-20 18:11:21 +0900

    auth hotfix

--- app/Http/Controllers/ViewController.php
+++ app/Http/Controllers/ViewController.php
@@ -9,7 +9,7 @@ class viewController extends Controller
     public function __construct()
     {
         $this->middleware('auth', [
-            'except'=>'index',
+            'except'=>['index','indexJA'],
         ]);
     }
     public function index()
