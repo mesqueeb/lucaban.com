e0220ca2a239d31d16f79d25a6720611a8cc893c
Luca Ban - Golden Birdy <mesqueeb@gmail.com>
2017-09-15 10:33:28 +0900

    tst

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,7 +28,7 @@ class AppServiceProvider extends ServiceProvider
     {
         if (env('APP_ENV') === 'production')
         {
-            URL::forceSchema('https');
+            \URL::forceSchema('https');
         }
     }
 }
